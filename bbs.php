458a329e51c334fd3542f29daf54792178827ada
Toyobijutsu-Prt <hatanaka@toyobijutsu-prt.co.jp>
2026-05-11 14:34:39 +0900

    うまくいくか今度こそ

--- bbs.php
+++ bbs.php
@@ -660,7 +660,8 @@ function strtocode($str) {
 }
 
 function errorprint($title,$erstr) {
-	echo "<HTML>\n<meta charset="UTF-8">\n<HEAD>\n";
+	echo "<HTML>\n<HEAD>\n";
+	echo "<META charset="UTF-8">\n";
 	echo "<LINK rel=stylesheet href=cgigarage.css type=text/css>\n";
 	echo "<TITLE>$title</TITLE>\n";
 	echo "</HEAD>\n";
